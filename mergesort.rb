0e58f8453245dd0174420e21f01a4beea4843a08
Nathan <BuddytheRat@gmail.com>
2015-07-28 21:23:02 -0500

    Mergesort completed.

--- mergesort.rb
+++ mergesort.rb
@@ -1,27 +1,20 @@
 def merge_sort(list)
-  puts list.to_s
   if list.size > 1
     m = (list.size-1)/2
     a, b = merge_sort(list[0..m]), merge_sort(list[m+1..-1])
-    puts "a: #{a[0]}, b: #{b[0]}"
   else
     return list
   end
-  sorted = Array.new
-  (a.size + b.size).times do
-    if (a.empty? && !b.empty?)
-      sorted += b
-      break
-    elsif (b.empty? && !a.empty?)
-      sorted += a
-      break
+  sorted = Array.new(list.size) do
+    if a.empty? || b.empty?
+      a.empty? ? b.shift : a.shift
     else
-      sorted << a[0] < b[0] ? a.shift : b.shift
+      a[0] < b[0] ? a.shift : b.shift
     end
   end
   sorted
 end
 
-silly_array = ((1..12).to_a + (20..30).to_a + (1..10).to_a).shuffle
+silly_array = ((1..100).to_a * 3).to_a.shuffle[0..15]
 puts silly_array.to_s
 puts merge_sort(silly_array).to_s
